3d7947d0454c5c7c9f4adbc4dd7486e1523eebba
Bernardo Donadio <bcdonadio@bcdonadio.com>
2023-05-15 02:05:56 -0300

    Update GH RSA key and add ECDSA, Ed25519

--- lib/functions/setupSSH.sh
+++ lib/functions/setupSSH.sh
@@ -26,18 +26,35 @@ function SetupGithubComSshKeys() {
     info "Adding github.com SSH keys"
     # Fetched out of band from
     # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
-    GITHUB_RSA_FINGERPRINT="SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8"
-    ssh-keyscan -t rsa github.com >/tmp/github.pub 2>/dev/null
+    GITHUB_RSA_FINGERPRINT="SHA256:uNiVztksCsDhcc0u9e8BujQXVUpKZIDTMczCvj3tD2s"
+    GITHUB_ECDSA_FINGERPRINT="SHA256:p2QAMXNIC1TJYWeIOttrVc98/R1BUFWu3/LiyKgUfQM"
+    GITHUB_ED25519_FINGERPRINT="SHA256:+DiY3wvvV6TuJJhbpZisF/zLDA0zPMSvHdkr4UvCOqU"
+    ssh-keyscan -t rsa github.com >/tmp/github_rsa.pub 2>/dev/null
+    ssh-keyscan -t ecdsa github.com >/tmp/github_ecdsa.pub 2>/dev/null
+    ssh-keyscan -t ed25519 github.com >/tmp/github_ed25519.pub 2>/dev/null
     if [[ "${SSH_INSECURE_NO_VERIFY_GITHUB_KEY}" == "true" ]]; then
       warn "Skipping github.com key verification and adding without checking fingerprint"
       mkdir -p ~/.ssh
-      cat /tmp/github.pub >>~/.ssh/known_hosts
-    elif [[ "$(ssh-keygen -lf /tmp/github.pub)" == "2048 ${GITHUB_RSA_FINGERPRINT} github.com (RSA)" ]]; then
-      info "Successfully verified github.com key"
-      mkdir -p ~/.ssh
-      cat /tmp/github.pub >>~/.ssh/known_hosts
+      cat /tmp/github_rsa.pub >>~/.ssh/known_hosts
+      cat /tmp/github_ecdsa.pub >>~/.ssh/known_hosts
+      cat /tmp/github_ed25519.pub >>~/.ssh/known_hosts
     else
-      error "Could not verify github.com key. SSH requests to github.com will likely fail."
+      mkdir -p ~/.ssh
+      if [[ "$(ssh-keygen -lf /tmp/github_rsa.pub)" == "3072 ${GITHUB_RSA_FINGERPRINT} github.com (RSA)" ]]; then
+        info "Successfully verified github.com RSA key"
+        cat /tmp/github_rsa.pub >>~/.ssh/known_hosts
+      fi
+      if [[ "$(ssh-keygen -lf /tmp/github_ecdsa.pub)" == "256 ${GITHUB_ECDSA_FINGERPRINT} github.com (ECDSA)" ]]; then
+        info "Successfully verified github.com ECDSA key"
+        cat /tmp/github_ecdsa.pub >>~/.ssh/known_hosts
+      fi
+      if [[ "$(ssh-keygen -lf /tmp/github_ed25519.pub)" == "256 ${GITHUB_ED25519_FINGERPRINT} github.com (ED25519)" ]]; then
+        info "Successfully verified github.com Ed25519 key"
+        cat /tmp/github_ed25519.pub >>~/.ssh/known_hosts
+      fi
+      if [[ -f "~/.ssh/known_hosts" ]]
+        error "Could not verify any github.com key. SSH requests to github.com will likely fail."
+      fi
     fi
   fi
 }
